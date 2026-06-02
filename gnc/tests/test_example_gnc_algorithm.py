40798c7ecfcfecc13fb41dac8a510b9895f6a00e
honzikschenk <nojschenk@gmail.com>
2026-06-02 19:31:41 -0400

    Adds gnc and perception skeleton projects

---
+++ gnc/tests/test_example_gnc_algorithm.py
@@ -0,0 +1,4 @@
+from gnc.src.example_gnc_algorithm import calculate_guidance
+
+def test_example_gnc_algorithm():
+    assert calculate_guidance() == 0.0
---
+++ perception/tests/test_example_perception.py
@@ -0,0 +1,4 @@
+from perception.src.example_perception import calculate_origin
+
+def test_example_perception():
+    assert calculate_origin() == (0.0, 0.0, 0.0)
