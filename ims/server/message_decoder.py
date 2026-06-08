ded5cb9c5c6bb81da5d5c11a7bdc3f7747bb31b0
Matthew Chan <matthew.chan0607@gmail.com>
2026-06-08 04:04:09 -0400

    Add decoder

--- ims/server/message_decoder.py
+++ ims/server/message_decoder.py
@@ -2,3 +2,13 @@
 Decodes incoming JSON bytes from airside_comms into typed msgspec Structs.
 Raises msgspec.DecodeError if the message doesn't match the expected schema.
 """
+
+import msgspec
+
+from utils.messages import AirsideMessage
+
+decoder = msgspec.json.Decoder(AirsideMessage)
+
+
+def decode(data: bytes) -> AirsideMessage:
+    return decoder.decode(data)
