3673b4ea8bbe339ff024bf4e98d160ff468f0677
Melodie X <mxiong@uwaterloo.ca>
2026-06-10 21:08:28 -0700

    Added reconnect attempt

--- ims/frontend/src/socket.js
+++ ims/frontend/src/socket.js
@@ -1,33 +1,47 @@
 // Parses the JSON string from message_encoder.py, and sends it
 // to the appropriate widgets. 
 
-
 //{"type": "attitude", "payload": {"roll": 0.1, "pitch":-0.05, "yaw":1.2, etc}
 
 const SOCKET_URL = 'SocketURL-ToBeFilled';
 
-const socket = new WebSocket(SOCKET_URL);
+let socket = null;
 const subscribers = {};
+let reconnectAttempts = 0;
+const BASE_DELAY = 1000;
 
-socket.onopen = () => {
-    console.log('Connected to RPi')
-}
+function connect() {
+  if (socket && (socket.readyState != WebSocket.CLOSED))
 
-socket.onmessage = (event) => {
-  const message = JSON.parse(event.data);
-  const { type, payload } = message;
-
-  if (subscribers[type]) {
-    subscribers[type].forEach(callback => callback(payload));
+  socket = new WebSocket(SOCKET_URL);
+  socket.onopen = () => {
+      console.log('Connected to RPi');
+      reconnectAttempts = 0;
   }
-}
+  
+  socket.onmessage = (event) => {
+    const message = JSON.parse(event.data);
+    const { type, payload } = message;
+  
+    if (subscribers[type]) {
+      subscribers[type].forEach(callback => callback(payload));
+    }
+  }
+  
+  socket.onerror = (error) => {
+    console.error(error);
+  }
+  
+  socket.onclose = () => {
+    console.warn('Websocket connection closed');
+    //Attempt to reconnect
+    console.log('Attemping to reconnect...');
+    const delay = BASE_DELAY * Math.pow(2, reconnectAttempts);
 
-socket.onerror = (error) => {
-  console.error(error);
-}
-
-socket.onclose = () => {
-  console.warn('Websocket connection closed');
+    setTimeout(() => {
+      connect();
+    }, delay);
+  }
 }
 
 export function subscribe(type, callback) {
@@ -41,3 +55,5 @@ export function unsubscribe(type, callback) {
     if (!subscribers[type]) return
     subscribers[type] = subscribers[type].filter(cb => cb !== callback)
 }
+
+connect();
