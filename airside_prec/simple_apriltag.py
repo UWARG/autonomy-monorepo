dca6e37872f7495cb3686147b83bbb6bf3fff0ce
VZhou07 <v23zhou@uwaterloo.ca>
2026-06-16 14:06:57 -0400

    fix apriltag python script

--- airside_prec/simple_apriltag.py
+++ airside_prec/simple_apriltag.py
@@ -3,10 +3,10 @@ from pupil_apriltags import Detector
 import yaml
 import numpy as np
 import time
-
+import os
 detector=Detector(families="tag36h11",nthreads=1,quad_decimate=1.0,quad_sigma=0.0,refine_edges=1,decode_sharpening=0.25,debug=0)
 
-with open("camera.yaml","r") as f:
+with open(os.path.join(os.path.dirname(__file__),"src","camera_info.yaml"),"r") as f:
     camera_params=yaml.safe_load(f)
     camera_matrix=np.array(camera_params["camera_matrix"]["data"]).reshape(3,3)
     dist_coeffs=np.array(camera_params["distortion_coefficients"]["data"]).reshape(1,14)
@@ -29,8 +29,8 @@ while True:
             success,rvec,tvec=cv2.solvePnP(object_points,result.corners,camera_matrix,dist_coeffs)
             if success:
                 print(tvec, f"tag id: {result.tag_id}")
-            cv2.rectangle(frame,tuple(result.corners[0].astype(int)),tuple(result.corners[2].astype(int)),(0,255,0),2)
-    cv2.imshow("frame",frame)
+            #cv2.rectangle(frame,tuple(result.corners[0].astype(int)),tuple(result.corners[2].astype(int)),(0,255,0),2)
+    #cv2.imshow("frame",frame)
     if cv2.waitKey(1) & 0xFF==ord('q'):
         break
     time.sleep(0.1)
