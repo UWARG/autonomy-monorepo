149386bfff93c2809394a57ea83697266d274b79
VZhou07 <v23zhou@uwaterloo.ca>
2026-07-14 00:44:21 -0400

    Add custom interfaces

--- prec_non_optimal/src/engine/engine/behaviours/landing.py
+++ prec_non_optimal/src/engine/engine/behaviours/landing.py
@@ -6,9 +6,7 @@ from __future__ import annotations
 
 import py_trees
 import rclpy.node
-from rclpy.action import ActionClient
-from mavros_msgs.action import LandingAction
-from action_msgs.msg import GoalStatus
+from custom_interfaces.srv import Landing
 
 class Landing(py_trees.behaviour.Behaviour):
     """
@@ -24,45 +22,30 @@ class Landing(py_trees.behaviour.Behaviour):
         """
 
         self._node = kwargs["node"]
-        self._landing_action_client=ActionClient(self._node, LandingAction, "/landing")
+        self._landing_client=self._node.create_client(Landing, "/landing")
 
     def initialise(self) -> None:
         """
         Called each time this behavior transitions from IDLE to RUNNING.
         """
-        self.status=None
-        while not self._landing_action_client.wait_for_server(timeout_sec=5.0):
-            self._node.get_logger().info("Waiting for landing action server")
-        goal=LandingAction.Goal()
-        self.goal_future=self._landing_action_client.send_goal_async(goal)
-        self.goal_future.add_done_callback(self.goal_response_callback)
+        self.success=None
+        while not self._landing_client.wait_for_service(timeout_sec=5.0):
+            self._node.get_logger().info("Waiting for landing service")
+        self.request=Landing.Request()
+        self.future=self._landing_client.call_async(self.request)
+        self.future.add_done_callback(self.response_callback)
     
-    def goal_response_callback(self, future):
-        goal_handle=future.result()
-        if not goal_handle.accepted:
-            self._node.get_logger().info('Goal rejected')
-            self.status=GoalStatus.STATUS_ABORTED
-            return
-        self._node.get_logger().info('Goal accepted')
-        self._goal_handle=goal_handle
-        self._result_future=goal_handle.get_result_async()
-        self._result_future.add_done_callback(self.get_result_callback)
-    
-    def get_result_callback(self, future):
+    def response_callback(self, future):
         response=future.result()
-        self.status=response.status
+        self.success=response.success
         
     def update(self) -> py_trees.common.Status:
         """
         Called on every tick while RUNNING.
         """
-        if self.status==GoalStatus.STATUS_ABORTED:
+        if not self.future.done() or self.success is None:
+            return py_trees.common.Status.RUNNING
+        if not self.success:
             self._node.get_logger().info("Landing Failed")
             return py_trees.common.Status.FAILURE
-        elif self.status==GoalStatus.STATUS_CANCELED:
-            self._node.get_logger().info("Landing Cancelled")
-            return py_trees.common.Status.FAILURE
-        if self.status==GoalStatus.STATUS_SUCCEEDED:
-            self._node.get_logger().info("Landing Succeeded")
-            return py_trees.common.Status.SUCCESS
-        return py_trees.common.Status.RUNNING
+        return py_trees.common.Status.SUCCESS
